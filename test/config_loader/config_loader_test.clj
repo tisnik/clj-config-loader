8718aea66e1f247054043d40f22aacb35646b336
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-07 13:46:46 +0100

    Tests for the config-loader module

---
+++ test/config_loader/config_loader_test.clj
@@ -0,0 +1,50 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns config-loader.config-loader-test
+  (:require [clojure.test :refer :all]
+            [config-loader.config-loader :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for functions existence
+;
+(deftest test-properties->map-existence
+    "Check that the config-loader.config-loader/properties->map definition exists."
+    (testing "if the config-loader.config-loader/properties->map definition exists."
+        (is (callable? 'config-loader.config-loader/properties->map))))
+
+
+(deftest test-load-property-file-existence
+    "Check that the config-loader.config-loader/load-property-file definition exists."
+    (testing "if the config-loader.config-loader/load-property-file definition exists."
+        (is (callable? 'config-loader.config-loader/load-property-file))))
+
+
+(deftest test-load-configuration-file-existence
+    "Check that the config-loader.config-loader/load-configuration-file definition exists."
+    (testing "if the config-loader.config-loader/load-configuration-file definition exists."
+        (is (callable? 'config-loader.config-loader/load-configuration-file))))
+
+;
+; Tests for functions behaviour
+;
+
+
