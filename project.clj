9a225c00c08a65f301e7c95be4bb7e0a287aa94d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-04 08:34:44 +0100

    Added lein-kibit as devel package

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
   :plugins [[codox "0.10.7"]
             [test2junit "1.1.0"]
             ;[lein-test-out "0.3.1"]
-            [lein-cloverage "1.0.7-SNAPSHOT"]]
+            [lein-cloverage "1.0.7-SNAPSHOT"]
+            [lein-kibit "0.1.8"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
